b16e5f164b489f4b34039249ce6cd7b9cfb5b651
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2022-12-17 22:45:01 +1300

    fix: issue with navbar items going from blank to filled suddenly

--- web/src/components/navigation/AvatarItem.tsx
+++ web/src/components/navigation/AvatarItem.tsx
@@ -1,4 +1,4 @@
-import { Show } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
 
 import { STORAGE_KEYS } from "../../utilities/constants";
 
@@ -6,24 +6,37 @@ export const AvatarItem = (props) => {
   const activeLinkClasses = "active nav-link";
   const inactiveLinkClasses = "nav-link";
 
-  const hasToken = window.localStorage.getItem(STORAGE_KEYS.Token);
-  const userInformation = JSON.parse(
-    window.localStorage.getItem(STORAGE_KEYS.User) ?? "{}"
+  const [hasToken, setHasToken] = createSignal(false);
+  const [userInformation, setUserInformation] = createSignal(
+    Object.create(null)
   );
+  const [isActive, setIsActive] = createSignal(false);
+  const [profileHref, setProfileHref] = createSignal("");
 
-  const profileHref = `/@${userInformation.username}`;
-  const isActive = window.location.pathname === profileHref;
+  onMount(() => {
+    if (window) {
+      const _userInformation = JSON.parse(
+        window.localStorage.getItem(STORAGE_KEYS.User) ?? "{}"
+      );
+      const _profileHref = `/@${_userInformation.username}`;
+
+      setHasToken(!!window?.localStorage.getItem(STORAGE_KEYS.Token));
+      setUserInformation(_userInformation);
+      setIsActive(window?.location.pathname === _profileHref);
+      setProfileHref(_profileHref);
+    }
+  });
 
   return (
     <>
       <Show when={!!hasToken}>
         <a
-          class={isActive ? activeLinkClasses : inactiveLinkClasses}
-          href={profileHref}
+          class={isActive() ? activeLinkClasses : inactiveLinkClasses}
+          href={profileHref()}
         >
-          <img class="user-pic" src={userInformation.image} />
+          <img class="user-pic" src={userInformation().image} />
           &nbsp;
-          {userInformation.username}
+          {userInformation().username}
         </a>
       </Show>
     </>
--- web/src/components/navigation/NavigationItem.tsx
+++ web/src/components/navigation/NavigationItem.tsx
@@ -1,26 +1,33 @@
-import { Show } from "solid-js";
+import { createSignal, onMount, Show } from "solid-js";
 
 import { STORAGE_KEYS } from "../../utilities/constants";
 
 export const NavigationItem = (props) => {
   const activeLinkClasses = "active nav-link";
   const inactiveLinkClasses = "nav-link";
-  const hasToken = window.localStorage.getItem(STORAGE_KEYS.Token);
+
+  const [hasToken, setHasToken] = createSignal(false);
+  const [isActive, setIsActive] = createSignal(false);
 
   const isVisible = () => {
-    if (props.requiresAuth && !hasToken) return false;
-    if (props.hideIfAuthenticated && hasToken) return false;
+    if (props.requiresAuth && !hasToken()) return false;
+    if (props.hideIfAuthenticated && hasToken()) return false;
 
     return true;
   };
 
-  const isActive = window.location.pathname === props.href;
+  onMount(() => {
+    if (window) {
+      setIsActive(window?.location.pathname === props.href);
+      setHasToken(!!window?.localStorage.getItem(STORAGE_KEYS.Token));
+    }
+  });
 
   return (
     <>
       <Show when={isVisible()}>
         <a
-          class={isActive ? activeLinkClasses : inactiveLinkClasses}
+          class={isActive() ? activeLinkClasses : inactiveLinkClasses}
           href={props.href}
         >
           {!!props.icon && (
