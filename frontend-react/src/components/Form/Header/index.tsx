788f97e2ef3715f4516260ff2c6b100ec11dd64a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-02-18 16:45:31 +1300

    fix: build errors

--- frontend-react/src/components/Form/Header/index.tsx
+++ frontend-react/src/components/Form/Header/index.tsx
@@ -1,4 +1,4 @@
-export const FormHeader = ({ title, subtitle }) => (
+export const FormHeader = ({ title, subtitle, subtitleHref }) => (
 	<>
 		<h1 className="text-xs-center">{title}</h1>
 		<p className="text-xs-center">
