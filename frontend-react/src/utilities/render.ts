53295f1ec257c511ffbccde53edc0f62af4e0619
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-02-21 22:28:03 +1300

    fix: building fine

--- frontend-react/src/utilities/render.ts
+++ frontend-react/src/utilities/render.ts
@@ -1,5 +1,5 @@
 import { MemoryRouter } from "react-router-dom";
 import { render as testingLibraryRender } from "@testing-library/react";
 
-export const render = (...args: any[]) =>
-	testingLibraryRender(...args, { wrapper: MemoryRouter });
+export const render = (component: any) =>
+	testingLibraryRender(component, { wrapper: MemoryRouter });
