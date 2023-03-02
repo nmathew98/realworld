5aa222fa570e3e1e91c8e32f2782117904549977
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-03-02 18:41:29 +1300

    chore: dev mode

--- backend-npostgres/src/routes/profiles/index.ts
+++ backend-npostgres/src/routes/profiles/index.ts
@@ -2,5 +2,5 @@ import { createRouter, eventHandler } from "h3";
 
 export const Profiles = createRouter().get(
 	"/profiles",
-	eventHandler(() => "Hi from profiles"),
+	eventHandler(() => "Hi from profiles!!!"),
 );
