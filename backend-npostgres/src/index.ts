1353a3d5626b601b77b2364e2db363d8d088accc
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-03-02 18:35:23 +1300

    feat: route information

--- backend-npostgres/src/index.ts
+++ backend-npostgres/src/index.ts
@@ -1,5 +1,13 @@
 import { createServer } from "node:http";
-import { createApp, toNodeListener } from "h3";
+import {
+	createApp,
+	eventHandler,
+	getHeaders,
+	getMethod,
+	getQuery,
+	getRouterParams,
+	toNodeListener,
+} from "h3";
 import { defineCorsEventHandler } from "@nozomuikuta/h3-cors";
 import consola from "consola";
 
@@ -10,6 +18,25 @@ const app = createApp();
 app.use(
 	defineCorsEventHandler({
 		/* ... */
+	}),
+);
+app.use(
+	"/",
+	eventHandler(e => {
+		const url = e.req.url;
+
+		consola.info(`[HTTP ${e.req.httpVersion}] ${getMethod(e)} ${url}`);
+		consola.info(`[${url}] headers: ${JSON.stringify(getHeaders(e), null, 2)}`);
+		consola.info(
+			`[${url}] query params: ${JSON.stringify(getQuery(e), null, 2)}`,
+		);
+		consola.info(
+			`[${url}] route params: ${JSON.stringify(
+				getRouterParams(e),
+				null,
+				2,
+			)}`,
+		);
 	}),
 );
 app.use(`/api/${apiInformation.version}`, Router.handler);
